df2b3237ccfb9abe30b163df86ad6e5befc049c2
akun <6awkun@gmail.com>
2014-05-27 22:41:29 +0800

    去除不用的依赖库

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['hieroglyph', 'sphinx.ext.autodoc', 'rst2pdf.pdfbuilder']
+extensions = ['hieroglyph', 'sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
